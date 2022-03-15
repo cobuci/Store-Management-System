419ce2e17cb55fd41f3fa9be7d920fa4267b2679
cobuci <cobuci80@gmail.com>
2022-03-15 00:04:58 -0300

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,7 +11,7 @@ class DashboardController extends Controller
     public function index()
     {
 
-        return view('admin.dashboard')->with('visitor', json_encode($result));
+        return view('admin.dashboard');
     }
 
 
