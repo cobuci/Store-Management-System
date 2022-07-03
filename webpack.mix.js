bdbae8ef45082da11f12a53385e7f7b739275c92
cobuci <cobuci80@gmail.com>
2022-07-03 19:02:49 -0300

    Dashboard Chart
    - Add revenue chart on dashboard

--- webpack.mix.js
+++ webpack.mix.js
@@ -21,6 +21,8 @@ if (mix.inProduction()) {
     mix.version();
 }
 
+mix.copy('node_modules/chart.js/dist/chart.js', 'public/chart.js/chart.js');
+
 mix
     .sass(
         "node_modules/bootstrap/scss/bootstrap.scss",
