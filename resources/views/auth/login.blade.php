6d6b2a0f70692e415237f1f038a7f27d251e5745
Victor Cobuci <cobuci80@gmail.com>
2023-09-15 17:03:18 -0300

    Composer update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,3 @@
-
 <head>
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -7,74 +6,72 @@
 
     <title>{{ env('APP_NAME') }}- Login</title>
 
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
-        <link rel="stylesheet" href="{{ asset('admin/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('admin/sb.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/sb.css') }}">
 
 </head>
 
 <body class="bg-gradient-primary">
 
-    <div class="container">
+<div class="container">
 
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+        <div class="col-xl-10 col-lg-12 col-md-9">
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                                    </div>
-                                    <form method="POST" action="{{ route('login') }}">
-                                        @csrf
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="email" name="email" :value="old('email')" required autofocus
-                                                placeholder="Enter Email Address...">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="password" placeholder="Password" name="password" required autocomplete="current-password">
-                                        </div>
-                                        <div class="form-group">
-                                            
-                                            <label for="remember_me" class="flex items-center">
-                                                <x-jet-checkbox id="remember_me" name="remember" />
-                                                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                                            </label>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </button>                                       
-                                     
-                                    </form>
-                                    <hr>
-                                  
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                 </div>
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <div class="form-group">
+                                        <input type="email" class="form-control form-control-user"
+                                               id="email" name="email" :value="old('email')" required autofocus
+                                               placeholder="Enter Email Address...">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user"
+                                               id="password" placeholder="Password" name="password" required
+                                               autocomplete="current-password">
+                                    </div>
+                                    <div class="form-group">
+
+                                        <label for="remember_me" class="flex items-center">
+                                            <x-jet-checkbox id="remember_me" name="remember"/>
+                                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                                        </label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        Login
+                                    </button>
+                                </form>
+                                <hr>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
 
         </div>
 
     </div>
 
-    <script src="{{ asset('admin/app.js') }}"></script>
-    <script src="{{ asset('admin/jquery.js') }}"></script>
+</div>
+
+
+<script src="{{ asset('admin/jquery.js') }}"></script>
 
 
 </body>
