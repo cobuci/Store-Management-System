ca2829fd8aa3a659688accf4fd0d0486ba5271a8
Victor Cobuci <cobuci80@gmail.com>
2023-10-04 02:38:33 -0300

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel([
-            "@css/app.css",
-            "@js/app.js",
-            "@js/chart.js",
+            "/resources/css/app.css",
+            "/resources/js/app.js",
+            "/resources/js/chart.js",
             "./node_modules/html2canvas/dist/html2canvas.js",
             "./node_modules/feather-icons/dist/feather.min.js",
         ]),
