ffc43b8c3f6a370ca3c7d75c725911194b0ddd63
Victor Cobuci <41239354+cobuci@users.noreply.github.com>
2023-10-21 02:57:33 -0300

    Statistics Component

--- app/Livewire/Pages/Statistics.php
+++ app/Livewire/Pages/Statistics.php
@@ -16,29 +16,8 @@ class Statistics extends Component
         'end' => '',
     ];
 
-    public $values = [
-        'cost' => 0,
-        'sale' => 0,
-        'profit' => 0,
-    ];
-    public $categories = [];
-    public $sales;
-    public $products = [];
-    public $payment_method = [
-        'Dinheiro' => 0,
-        'Credito' => 0,
-        'Debito' => 0,
-        'PIX' => 0,
-        'Outros' => 0,
-    ];
 
-    public function mount()
-    {
-        $this->categories = Category::all();
-    }
-
-    public function updatedDate()
-    {
+    public function updatedDate(){
 
         $firstSale = Sale::orderBy('created_at', 'asc')->first();
         if (empty($this->date['start'])) {
@@ -47,106 +26,10 @@ class Statistics extends Component
         if (empty($this->date['end'])) {
             $this->date['end'] = date('Y-m-d', strtotime('+1 day'));
         }
-        $this->values['cost'] = $this->costValueTotal();
-        $this->values['sale'] = $this->saleValueTotal();
-        $this->values['profit'] = $this->profitValue();
-        $this->sales = $this->getSales();
-        $this->products = $this->getProducts();
-        $amounts = array_column($this->products, 'amount');
-        array_multisort($amounts, SORT_DESC, $this->products);
-        $this->paymentMethodTotal();
+
+        $this->dispatch('dateUpdated',$this->date);
     }
-
-    public function costValueTotal()
-    {
-        return Sale::where('created_at', '>=', $this->date['start'])
-            ->where('created_at', '<=', $this->date['end'])
-            ->sum('cost');
-    }
-
-    public function saleValueTotal()
-    {
-        return Sale::where('created_at', '>=', $this->date['start'])
-            ->where('created_at', '<=', $this->date['end'])
-            ->sum('price');
-    }
-
-    public function profitValue()
-    {
-        return $this->saleValueTotal() - $this->costValueTotal();
-    }
-
-    public function getSales()
-    {
-        return Sale::with('orders')
-            ->where('created_at', '>=', $this->date['start'])
-            ->where('created_at', '<=', $this->date['end'])
-            ->get();
-    }
-
-    public function getProducts()
-    {
-        $products = [
-            ['id' => '',
-                'name' => '',
-                'amount' => '',
-                'brand' => '',
-                'cost' => '',
-                'sale' => '',
-                'weight' => '',
-                'profit' => '',
-                'category_id' => ''
-            ]
-        ];
-        foreach ($this->sales as $sale) {
-
-            foreach ($sale->orders as $order) {
-                $productId = $order->product_id;
-                $categoryId = Product::find($productId)?->category_id;
-
-                // Verifica se o produto já está no array $products
-                if (isset($products[$productId])) {
-                    // Se sim, atualiza as quantidades e custos
-                    $products[$productId]['amount'] += $order->amount;
-                    $products[$productId]['cost'] += $order->unit_cost * $order->amount;
-                    $products[$productId]['sale'] += $order->unit_price * $order->amount;
-                    $products[$productId]['profit'] += ($order->unit_price * $order->amount) - ($order->unit_cost * $order->amount);
-                } else {
-                    // Se não, cria um novo registro para o produto
-                    $products[$productId] = [
-                        'id' => $productId,
-                        'name' => $order->product_name,
-                        'brand' => $order->product_brand,
-                        'amount' => $order->amount,
-                        'weight' => $order->weight,
-                        'cost' => $order->unit_cost * $order->amount,
-                        'sale' => $order->unit_price * $order->amount,
-                        'profit' => ($order->unit_price * $order->amount) - ($order->unit_cost * $order->amount),
-                        'category_id' => $categoryId,
-                    ];
-                }
-
-
-            }
-        }
-        return $products;
-
-    }
-
-    public function paymentMethodTotal()
-    {
-
-        $methods = Sale::select('payment_method', DB::raw('SUM(price) as total_price'))
-            ->where('created_at', '>=', $this->date['start'])
-            ->where('created_at', '<=', $this->date['end'])
-            ->groupBy('payment_method')
-            ->get();
-
-        foreach ($methods as $method) {
-            $this->payment_method[$method->payment_method] = $method->total_price;
-        }
-    }
-
+    
     public function render()
     {
 
---
+++ resources/views/admin/components/statistics.blade.php
@@ -0,0 +1,126 @@
+<div>
+   
+    <ul class="mb-5 flex list-none flex-col flex-wrap pl-0 md:flex-row" role="tablist" data-te-nav-ref>
+        {{--        Valores         --}}
+        <li role="presentation">
+            <a class="
+            my-2 block rounded-lg bg-neutral-100 px-7
+            border-black border-2
+            pb-3.5 pt-4 text-xs font-medium uppercase
+            leading-tight
+            cursor-pointer
+            text-neutral-500
+            data-[te-nav-active]:!bg-gray-400
+            dark:data-[te-nav-active]:!bg-gray-700
+            data-[te-nav-active]:text-black
+            dark:bg-gray-900
+            dark:text-white
+            dark:data-[te-nav-active]:text-white
+            md:mr-4"
+                data-te-toggle="pill" data-te-target="#pills-values" data-te-nav-active role="tab"
+                aria-controls="pills-values" aria-selected="true">Valores</a>
+        </li>
+        {{--        Produtos         --}}
+        <li role="presentation">
+            <a class="
+            my-2 block rounded-lg bg-neutral-100 px-7
+            border-black border-2
+            pb-3.5 pt-4 text-xs font-medium uppercase
+            leading-tight
+            cursor-pointer
+            text-neutral-500
+            data-[te-nav-active]:!bg-gray-400
+            dark:data-[te-nav-active]:!bg-gray-700
+            data-[te-nav-active]:text-black
+            dark:bg-gray-900
+            dark:text-white
+            dark:data-[te-nav-active]:text-white
+            md:mr-4"
+                data-te-toggle="pill" data-te-target="#pills-products" role="tab" aria-controls="pills-products"
+                aria-selected="false">Produtos</a>
+        </li>
+    </ul>
+    {{--        Valores         --}}
+    <div class="hidden opacity-100 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
+        id="pills-values" role="tabpanel" data-te-tab-active>
+        <div
+            class="flex flex-col w-full p-4 min-w-fit bg-white/[0.8] dark:bg-gray-900 rounded-lg border-2 border-black ">
+            <div class="mb-2">
+                <span class=" font-medium">
+                    Periodo: {{ $date['start'] }} - {{ $date['end'] }}
+                </span>
+            </div>
+            <div class="flex justify-between w-full min-w-fit flex-wrap">
+                <span class="flex flex-nowrap font-medium">
+                    Custo Total: R$ {{ $values['cost'] }}
+                </span>
+                <span class="flex flex-nowrap font-medium">
+                    Venda Total: R$ {{ $values['sale'] }}
+                </span>
+                <span class="flex flex-nowrap font-medium">
+                    Lucro Total: R$ {{ $values['profit'] }}
+                </span>
+            </div>
+            <div class="overflow-hidden">
+
+                "@json(array_keys($payment_method))"
+                "@json(array_values($payment_method))"
+                @foreach ($payment_method as $key => $value)
+                    <div class="flex justify-between w-full min-w-fit flex-wrap">
+                        <span class="flex flex-nowrap font-medium">
+                            {{ $key }}: R$ {{ $value }}
+                        </span>
+                    </div>
+                @endforeach
+
+            </div>
+
+        </div>
+    </div>
+    {{--        Produtos         --}}
+    <div class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
+        id="pills-products" role="tabpanel">
+        <div class=" w-full  border-2 border-black rounded-t-lg ">
+            @foreach ($categories as $category)
+                <div class="bg-white/[0.8] dark:bg-gray-900" x-data="{ open: false }">
+                    <div class="p-2 cursor-pointer dark:hover:bg-gray-700 hover:bg-gray-400" @click="open = ! open">
+                        <i class="{{ $category->icon }} text-xl mx-2"> </i>
+                        <span class="font-bold text-xl"> {{ $category->name }}</span>
+                    </div>
+                    <div class="overflow-auto" x-show="open" x-transition>
+                        <table class="w-full whitespace-nowrap">
+                            <thead class="border-b border-1">
+                                <tr class="bg-white/[0.1]">
+                                    <th class="border-r">#</th>
+                                    <th class="border-r">Produto</th>
+                                    <th class="border-r">Marca</th>
+                                    <th class="border-r">Peso</th>
+                                    <th class="border-r">Qt. Vendida</th>
+                                    <th class="border-r">Custo Total</th>
+                                    <th class="border-r">Venda Total</th>
+                                    <th class="border-r">Lucro</th>
+                                </tr>
+                            </thead>
+                            <tbody class="dark:bg-gray-700">
+                                @foreach ($products as $product)
+                                    @if ($product['category_id'] == $category->id)
+                                        <tr class="border-b dark:hover:bg-gray-600 hover:bg-gray-400">
+                                            <td class=" px-2 py-4">{{ $product['id'] }}</td>
+                                            <td class=" px-2 py-4">{{ $product['name'] }}</td>
+                                            <td class=" px-2 py-4">{{ $product['brand'] }}</td>
+                                            <td class=" px-2 py-4">{{ $product['weight'] }}</td>
+                                            <td class=" px-2 py-4">{{ $product['amount'] }}</td>
+                                            <td class=" px-2 py-4">R$ {{ $product['cost'] }}</td>
+                                            <td class=" px-2 py-4">R$ {{ $product['sale'] }}</td>
+                                            <td class=" px-2 py-4">R$ {{ $product['profit'] }}</td>
+                                        </tr>
+                                    @endif
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
