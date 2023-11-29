396c97da75c4490da829b0a986d3e4c29ef5286b
Victor Cobuci <41239354+cobuci@users.noreply.github.com>
2023-11-29 19:11:11 -0300

    Update invoice.blade.php

--- resources/views/admin/invoice.blade.php
+++ resources/views/admin/invoice.blade.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js" integrity="sha512-BNaRQnYJYiPSqHHDb58B0yaPfCu+Wgds8Gp/gU33kqBtgNS4tSPHuGibyoeqMV/TJlSKda6FXzoEyYGjTe+vXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <title>invoice - Garagem 46</title>
+    <title>invoice - {{ config("app.name") }} </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
           integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -42,7 +42,7 @@
                     <div class="invoice-title">
 
                         <div class="mb-4">
-                            <h2 class="mb-1 text-muted"> Garagem 46 </h2>
+                            <h2 class="mb-1 text-muted"> {{ config("app.name")}} </h2>
                         </div>
                     </div>
                     <hr class="my-4">
