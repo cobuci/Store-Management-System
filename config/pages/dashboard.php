ba7198b9e4660dcb9a63929b945fec3e29e2352a
Victor Cobuci <41239354+cobuci@users.noreply.github.com>
2023-12-01 20:47:15 -0300

    Update dashboard.php

--- config/pages/dashboard.php
+++ config/pages/dashboard.php
@@ -1,4 +1,4 @@
 <?php
 return [
-    'monthsChart' => 13, //Quantidade de meses que mostrará no grafico da Dashboard
+    'monthsChart' => 13, //Quantidade de meses que mostrará no gráfico da Dashboard
 ];
