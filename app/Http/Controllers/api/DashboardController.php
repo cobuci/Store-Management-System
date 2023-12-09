89d82693842b3862363bf3a3fc2865806c6447b9
Victor Cobuci <41239354+cobuci@users.noreply.github.com>
2023-12-09 06:21:06 -0300

    feat: add new data api

---
+++ app/Http/Controllers/api/DashboardController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cashier;
+use App\Models\Sale;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function balance(): array
+    {
+        $cashier = Cashier::find(1);
+        $due = Sale::totalDue();
+
+        $result['balance'] = $cashier->balance - $due;
+
+        return $result;
+    }
+
+    public function getSalesIncomeForLastDays(Int $date = 0): array
+    {
+        $sales = DB::table('sales')
+            ->select(
+                DB::raw('DATE(created_at) as date'),
+                DB::raw('SUM(price) as total_price'),
+            )
+            ->groupBy('date')
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        $result['sales'] = $sales[$date]->total_price ?? "0";
+
+        return $result;
+    }
+
+    public function getSalesIncomeForLastMonth(Int $date = 0): array
+    {
+        $sales = DB::table('sales')
+            ->select(
+                DB::raw('count(id) as `data`'),
+                DB::raw("DATE_FORMAT(created_at, '%m-%Y') new_date"),
+                DB::raw('YEAR(created_at) year, MONTH(created_at) month'),
+                DB::raw('SUM(price) as total_price')
+            )
+            ->groupBy('year', 'month')
+            ->orderBy('id', 'desc')
+            ->get();
+
+        $result['sales'] = $sales[$date]->total_price ?? "0";
+
+        return $result;
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 
 use App\Http\Controllers\api\CategoryController;
+use App\Http\Controllers\api\DashboardController;
 use App\Http\Controllers\api\ProductController;
+use App\Http\Controllers\CashierController;
 use App\Http\Middleware\ApiAuth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +12,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(ApiAuth::class)->group(function () {
     Route::prefix('v1')->group(function () {
+
+        Route::get('balance', [DashboardController::class, 'balance']);
+        Route::get('income/daily/{date?}', [DashboardController::class, 'getSalesIncomeForLastDays']);
+        Route::get('income/monthly/{date?}', [DashboardController::class, 'getSalesIncomeForLastMonth']);
+
         Route::get('products', [ProductController::class, 'index']);
         Route::get('products/{id}', [ProductController::class, 'show']);
         Route::get('products/category/{id}', [ProductController::class, 'productsByCategory']);
